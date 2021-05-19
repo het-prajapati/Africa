afff58b119020fda1040b1585abe2f48195df259
Het Prajapati <hprajapati@albany.edu>
2021-05-19 18:20:57 +0530

    Added TaskView with json application of covers.json file

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Het Prajapati on 5/19/21.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
